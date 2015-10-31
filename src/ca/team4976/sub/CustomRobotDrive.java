35de4264be5bdf6cea58a4e1aab0f2d0b47a01ff
Marc Levesque <Qormix@gmail.com>
2015-10-31 15:03:02 -0400

    Added PID

--- src/ca/team4976/sub/CustomRobotDrive.java
+++ src/ca/team4976/sub/CustomRobotDrive.java
@@ -19,4 +19,27 @@ public class CustomRobotDrive {
         Output.Motor.DRIVE_RIGHT_1.set(right);
         Output.Motor.DRIVE_RIGHT_2.set(right);
     }
+
+    public class PID {
+
+        double kp, ki, kd;
+
+        double previousError = 0;
+        double i = 0;
+        double d = 0;
+
+        public PID(double kp, double ki, double kd) { this.kp = kp; this.ki = ki; this.kd = kd; }
+
+        public double getPID(double error, double speed, long startTimeMillis) {
+
+            double dt = ((System.currentTimeMillis() - startTimeMillis) / 1000);
+
+            i += error * dt;
+            d = (error - previousError) / dt;
+
+            previousError = error;
+
+            return (error * kp) + (i * ki) + (d * kd);
+        }
+    }
 }
